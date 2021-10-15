d77ab2671f5f4272e7868a3ea86da0c78f072d65
Lars Bürger <lars.buerger@freenet.de>
2021-10-15 13:10:32 +0200

    secondchange

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,32 @@
-@main def hello: Unit = 
+object demo{
+@main def test: Unit = 
   println("Hello world!")
-  println(msg)
+  
+  val x = 10
 
-def msg = "I was compiled by Scala 3. :)"
+  val y = 20
+
+  printf("Value of X = %d\n", x)
+  printf("Value of Y = %d\n", y)
+
+  val z = x + y
+  printf("Value of Z = %d\n", z)
+
+
+  def msg = "I was compiled by Scala 3. :)"
+
+  def addInt(a:Int, b:Int) : Int = {
+  var sum:Int = 0
+  sum = a + b
+
+  return sum
+  }
+  val sum = addInt(4, 6)
+  printf("sum-function: %d\n", sum)
+
+  def strtest = """This is a test-string 
+its also a multiline string
+this is very funny to programm :)
+"""
+  printf(strtest)
+}
