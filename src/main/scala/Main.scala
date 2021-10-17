a136002411131ce8547248980ac8000b2303492c
nickhoess <nick@futuresoftware.de>
2021-10-17 14:49:01 +0200

    passt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,32 @@
-object demo{
-@main def test: Unit = 
-  println("Hello world!")
-  
-  val x = 10
+object demo {
+  @main def test: Unit =
+    println("Hello world!")
 
-  val y = 20
+    val x = 10
 
-  printf("Value of X = %d\n", x)
-  printf("Value of Y = %d\n", y)
+    val y = 20
 
-  val z = x + y
-  printf("Value of Z = %d\n", z)
+    printf("Value of X = %d\n", x)
+    printf("Value of Y = %d\n", y)
 
+    val z = x + y
+    printf("Value of Z = %d\n", z)
 
-  def msg = "I was compiled by Scala 3. :)"
+    def msg = "I was compiled by Scala 3. :)"
 
-  def addInt(a:Int, b:Int) : Int = {
-  var sum:Int = 0
-  sum = a + b
+    def addInt(a: Int, b: Int): Int = {
+      var sum: Int = 0
+      sum = a + b
 
-  return sum
-  }
-  val sum = addInt(4, 6)
-  printf("sum-function: %d\n", sum)
+      return sum
+    }
+    val sum = addInt(4, 6)
+    printf("sum-function: %d\n", sum)
 
-  def strtest = """This is a test-string 
+    def strtest = """This is a test-string 
 its also a multiline string
 this is very funny to programm :)
 """
-  printf(strtest)
+    printf(strtest)
+    printf("bin drinn")
 }
